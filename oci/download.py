c0dd0aa94741324c36e829d6280274ffa4eeca8d
Ryan Weiler <ryanweiler92@gmail.com>
2025-03-17 21:25:27 -0400

    OCI images structure

---
+++ oci/download.py
@@ -0,0 +1,16 @@
+from huggingface_hub import snapshot_download
+
+
+def download_model_files():
+
+    model_repo_id = "microsoft/Florence-2-large"
+
+    short_name = "florence-2-large"
+
+    local_model_dir = f"./oci/{short_name}/model_files"
+
+    snapshot_download(repo_id=model_repo_id, local_dir=local_model_dir)
+
+
+if __name__ == "__main__":
+    download_model_files()
---
+++ oci/florence-2-large/README.md
@@ -0,0 +1,6 @@
+# Florence 2 Large
+
+- `docker build -t myuser/florence-2-large:1.0 oci/florence-2-large/`
+
+- `docker run -it --name florence-2-large myuser/florence-2-large:1.0 /bin/sh`
+`
