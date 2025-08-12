4e0faebb008900c36b0229d94cd8ea1db5897abd
Weiler, Ryan <ryanweiler92@gmail.com>
2025-08-12 13:46:57 -0400

    updating model build gha

--- oci/download.py
+++ oci/download.py
@@ -3,9 +3,9 @@ from huggingface_hub import snapshot_download
 
 def download_model_files():
 
-    model_repo_id = "hexgrad/Kokoro-82M"
+    model_repo_id = "openai/whisper-large-v3"
 
-    short_name = "kokoro-82m"
+    short_name = "whisper-large-v3"
 
     local_model_dir = f"./oci/{short_name}"
 
